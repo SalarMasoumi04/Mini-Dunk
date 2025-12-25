a7150ba3b4f478e1e04bf36d5f6cf95764426d9c
S. Salar . Masoumi <113671169+SalarMasoumi04@users.noreply.github.com>
2025-12-26 00:54:57 +0330

    Basket
    Add Score when ball get into

---
+++ Basket Script.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class BasketScore : MonoBehaviour
+{
+    public int scoreValue = 1; // امتیاز هر گل
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Ball")) // توپ باید تگ "Ball" داشته باشه
+        {
+            // اینجا امتیاز اضافه کن
+            GameManager.instance.AddScore(scoreValue);
+
+            Debug.Log("امتیاز! توپ وارد سبد شد.");
+        }
+    }
+}
