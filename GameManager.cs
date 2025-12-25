963c842d83aee19a16f6e74769ff73bd7237fd8f
S. Salar . Masoumi <113671169+SalarMasoumi04@users.noreply.github.com>
2025-12-26 00:55:29 +0330

    Manager
    Game Manager

---
+++ GameManager.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+    private int score = 0;
+
+    private void Awake()
+    {
+        if (instance == null)
+            instance = this;
+    }
+
+    public void AddScore(int value)
+    {
+        score += value;
+        Debug.Log("امتیاز کل: " + score);
+        // اینجا می‌تونی UI رو آپدیت کنی
+    }
+}
